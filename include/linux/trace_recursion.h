03452a613452fbb51c2e8a283341db434f930f71
Steven Rostedt (VMware) <rostedt@goodmis.org>
2019-04-22 17:41:44 -0400

    function_graph: Move graph notrace bit to shadow stack global var
    The use of the task->trace_recursion for the logic used for the function
    graph no-trace was a bit of an abuse of that variable. Now that there exists
    global vars that are per stack for registered graph traces, use that instead.
    
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- include/linux/trace_recursion.h
+++ include/linux/trace_recursion.h
@@ -58,13 +58,6 @@ enum {
 	TRACE_IRQ_BIT,
 
 	/*
-	 * To implement set_graph_notrace, if this bit is set, we ignore
-	 * function graph tracing of called functions, until the return
-	 * function is called to clear it.
-	 */
-	TRACE_GRAPH_NOTRACE_BIT,
-
-	/*
 	 * When transitioning between context, the preempt_count() may
 	 * not be correct. Allow for a single recursion to cover this case.
 	 */
