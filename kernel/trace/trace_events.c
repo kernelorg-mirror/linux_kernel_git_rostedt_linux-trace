kernelorg-mirror/linux_kernel_git_trace_linux-trace

tracefs: Add show_events_dentries
Add a file in tracefs that shows the "events" allocated entries and the
dentries that are attached to them. This is used to see what dentries have
been dynamically allocated as well as their current ref counts.

 ~# cat /sys/kernel/tracing/events/sched/sched_switch/enable
 0
 ~# grep -A4 sched_switch /sys/kernel/tracing/show_events_dentries
 sched_switch/ dentry: (1)
 enable dentry: (0)
 id
 filter
 trigger

The first value is the name of the file or directory. If a dentry is
allocated, then a "dentry: (<ref-count>)" is displayed showing the address
of the dentry as well as its ref count.

Link: https://lore.kernel.org/linux-trace-kernel/20230913113628.172907b7@gandalf.local.home

Cc: Masami Hiramatsu <mhiramat@kernel.org>
Cc: Mark Rutland <mark.rutland@arm.com>
Cc: Ajay Kaher <akaher@vmware.com>
Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>

kernel/trace/trace_events.c
@@ -3639,6 +3639,9 @@ create_event_toplevel_files(struct dentry *parent, struct trace_array *tr)
 		return -ENOMEM;
 	}
 
+	trace_create_file("show_events_dentries", TRACE_MODE_READ, parent, d_events,
+			  &eventfs_show_dentry_fops);
+
 	error = eventfs_add_events_file("enable", TRACE_MODE_WRITE, d_events,
 				  tr, &ftrace_tr_enable_fops);
 	if (error)

kernelorg-mirror/linux_kernel_git_rostedt_linux-trace

Cc: Mark Rutland  <mark.rutland@arm.com>

kernel/trace/trace_events.c
@@ -3802,6 +3802,9 @@ create_event_toplevel_files(struct dentry *parent, struct trace_array *tr)
 		return -ENOMEM;
 	}
 
+	trace_create_file("show_events_dentries", TRACE_MODE_READ, parent, e_events,
+			  &eventfs_show_dentry_fops);
+
 	/* There are not as crucial, just warn if they are not created */
 
 	trace_create_file("set_event_pid", TRACE_MODE_WRITE, parent,