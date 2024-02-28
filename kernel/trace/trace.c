ea07152f8a7ef799ad8b376077a783101bf60527
Steven Rostedt (Google) <rostedt@goodmis.org>
2024-02-28 14:28:05 -0500

    tracing: Add option to use memmap boot memory for trace boot instance
    Add an option to the trace_instance kernel command line parameter that
    allows it to use the reserved memory from memmap boot parameter.
    
      memmap=12M*4096:trace  trace_instance=boot_mapped@trace
    
    The above will allocate 12 megs with 4096 alignment and label it "trace".
    The second parameter will create a "boot_mapped" instance and use the
    memory reserved and labeled as "trace" as the memory for the ring buffer.
    
    That will create an instance called "boot_mapped":
    
      /sys/kernel/tracing/instances/boot_mapped
    
    Note, because the ring buffer is using a defined memory ranged, it will
    act just like a memory mapped ring buffer. It will not have a snapshot
    buffer, as it can't swap out the buffer. The snapshot files as well as any
    tracers that uses a snapshot will not be present in the boot_mapped
    instance.
    
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -9513,6 +9513,31 @@ out_unlock:
 	return ret;
 }
 
+static u64 map_pages(u64 start, u64 size)
+{
+	struct page **pages;
+	phys_addr_t page_start;
+	unsigned int page_count;
+	unsigned int i;
+	void *vaddr;
+
+	page_count = DIV_ROUND_UP(size, PAGE_SIZE);
+
+	page_start = start;
+	pages = kmalloc_array(page_count, sizeof(struct page *), GFP_KERNEL);
+	if (!pages)
+		return 0;
+
+	for (i = 0; i < page_count; i++) {
+		phys_addr_t addr = page_start + i * PAGE_SIZE;
+		pages[i] = pfn_to_page(addr >> PAGE_SHIFT);
+	}
+	vaddr = vmap(pages, page_count, VM_MAP, PAGE_KERNEL);
+	kfree(pages);
+
+	return (u64)(unsigned long)vaddr;
+}
+
 /**
  * trace_array_get_by_name - Create/Lookup a trace array, given its name.
  * @name: The name of the trace array to be looked up/created.
@@ -10359,6 +10384,7 @@ __init static void enable_instances(void)
 {
 	struct trace_array *tr;
 	char *curr_str;
+	char *name;
 	char *str;
 	char *tok;
 
@@ -10367,19 +10393,33 @@ __init static void enable_instances(void)
 	str = boot_instance_info;
 
 	while ((curr_str = strsep(&str, "\t"))) {
+		u64 start = 0;
+		u64 size = 0;
 
 		tok = strsep(&curr_str, ",");
+		name = strsep(&tok, "@");
 
 		if (IS_ENABLED(CONFIG_TRACER_MAX_TRACE))
 			do_allocate_snapshot(tok);
 
-		tr = trace_array_get_by_name(tok, NULL);
+		if (tok) {
+			if (memmap_named(tok, &start, &size))
+				start = map_pages(start, size);
+			if (!start) {
+				pr_warn("Failed to map boot instance %s to %s\n", name, tok);
+				continue;
+			}
+		}
+
+		tr = trace_array_create_systems(name, NULL, start, size);
 		if (!tr) {
 			pr_warn("Failed to create instance buffer %s\n", curr_str);
 			continue;
 		}
-		/* Allow user space to delete it */
-		trace_array_put(tr);
+
+		/* Do not allow mapped buffers to be deleted */
+		if (start)
+			tr->ref++;
 
 		while ((tok = strsep(&curr_str, ","))) {
 			early_enable_events(tr, tok, true);
