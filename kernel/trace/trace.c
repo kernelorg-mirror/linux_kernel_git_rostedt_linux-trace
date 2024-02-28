06c8dd8d86240bdfbab979b2a24e357380c2d742
Steven Rostedt (Google) <rostedt@goodmis.org>
2024-02-28 14:28:05 -0500

    tracing: Allow boot instances to use reserve_mem boot memory
    Allow boot instances to use memory reserved by the reserve_mem boot
    option.
    
      reserve_mem=12M:4096:trace  trace_instance=boot_mapped@trace
    
    The above will allocate 12 megs with 4096 alignment and label it "trace".
    The second parameter will create a "boot_mapped" instance and use the
    memory reserved and labeled as "trace" as the memory for the ring buffer.
    
    That will create an instance called "boot_mapped":
    
      /sys/kernel/tracing/instances/boot_mapped
    
    Note, because the ring buffer is using a defined memory ranged, it will
    act just like a memory mapped ring buffer. It will not have a snapshot
    buffer, as it can't swap out the buffer. The snapshot files as well as any
    tracers that uses a snapshot will not be present in the boot_mapped
    instance.
    
    Also note that reserve_mem is not reliable in acquiring the same physical
    memory at each soft reboot. It is possible that KALSR could map the kernel
    at the previous boot memory location forcing the reserve_mem to return a
    different memory location. In this case, the previous ring buffer will be
    lost.
    
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -10604,16 +10604,14 @@ __init static void enable_instances(void)
 
 		tok = strsep(&curr_str, ",");
 		name = strsep(&tok, "@");
-		if (tok) {
+
+		if (tok && isdigit(*tok)) {
 			start = memparse(tok, &tok);
 			if (!start) {
 				pr_warn("Tracing: Invalid boot instance address for %s\n",
 					name);
 				continue;
 			}
-		}
-
-		if (start) {
 			if (*tok != ':') {
 				pr_warn("Tracing: No size specified for instance %s\n", name);
 				continue;
@@ -10625,10 +10623,19 @@ __init static void enable_instances(void)
 					name);
 				continue;
 			}
+		} else if (tok) {
+			if (!reserve_mem_find_by_name(tok, &start, &size)) {
+				start = 0;
+				pr_warn("Failed to map boot instance %s to %s\n", name, tok);
+				continue;
+			}
+		}
+
+		if (start) {
 			addr = map_pages(start, size);
 			if (addr) {
 				pr_info("Tracing: mapped boot instance %s at physical memory 0x%lx of size 0x%lx\n",
-					name, start, size);
+					name, addr, size);
 			} else {
 				pr_warn("Tracing: Failed to map boot instance %s\n", name);
 				continue;
