2c6359c179170ba64a69a324520e12e9f913e16c
Steven Rostedt <rostedt@goodmis.org>
2025-02-05 11:25:39 -0500

    module: Add module_for_each_mod() function
    The tracing system needs a way to save all the currently loaded modules
    and their addresses into persistent memory so that it can evaluate the
    addresses on a reboot from a crash. When the persistent memory trace
    starts, it will load the module addresses and names into the persistent
    memory. To do so, it will call the module_for_each_mod() function and pass
    it a function and data structure to get called on each loaded module. Then
    it can record the memory.
    
    This only implements that function.
    
    Cc: Luis Chamberlain <mcgrof@kernel.org>
    Cc: Petr Pavlu <petr.pavlu@suse.com>
    Cc: Sami Tolvanen <samitolvanen@google.com>
    Cc: Daniel Gomez <da.gomez@samsung.com>
    Cc: linux-modules@vger.kernel.org
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>

--- kernel/module/main.c
+++ kernel/module/main.c
@@ -3809,6 +3809,20 @@ bool is_module_text_address(unsigned long addr)
 	return ret;
 }
 
+void module_for_each_mod(int(*func)(struct module *mod, void *data), void *data)
+{
+	struct module *mod;
+
+	preempt_disable();
+	list_for_each_entry_rcu(mod, &modules, list) {
+		if (mod->state == MODULE_STATE_UNFORMED)
+			continue;
+		if (func(mod, data))
+			break;
+	}
+	preempt_enable();
+}
+
 /**
  * __module_text_address() - get the module whose code contains an address.
  * @addr: the address.
