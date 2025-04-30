8da56e20d3413bd7c766624f1a436c869be92fa1
Josh Poimboeuf <jpoimboe@kernel.org>
2025-04-30 15:58:00 -0400

    perf: Support deferred user callchains
    Use the new unwind_deferred_trace() interface (if available) to defer
    unwinds to task context.  This will allow the use of .sframe (when it
    becomes available) and also prevents duplicate userspace unwinds.
    
    Cc: Masami Hiramatsu <mhiramat@kernel.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Arnaldo Carvalho de Melo <acme@kernel.org>
    Cc: Indu Bhagat <indu.bhagat@oracle.com>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Ian Rogers <irogers@google.com>
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Mark Brown <broonie@kernel.org>
    Cc: Jordan Rome <jordalgo@meta.com>
    Cc: Sam James <sam@gentoo.org>
    Cc: Andrii Nakryiko <andrii.nakryiko@gmail.com>
    Cc: Jens Remus <jremus@linux.ibm.com>
    Cc: Florian Weimer <fweimer@redhat.com>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Weinan Liu <wnliu@google.com>
    Cc: Blake Jones <blakejones@google.com>
    Cc: Beau Belgrave <beaub@linux.microsoft.com>
    Cc: "Jose E. Marchesi" <jemarch@gnu.org>
    Cc: Alexander Aring <aahringo@redhat.com>
    Link: https://lore.kernel.org/20250430200109.845004761@goodmis.org
    Suggested-by: Peter Zijlstra <peterz@infradead.org>
    Co-developed-by: Steven Rostedt (Google) <rostedt@goodmis.org>
    Signed-off-by: Josh Poimboeuf <jpoimboe@kernel.org>
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -62,6 +62,7 @@ struct perf_guest_info_callbacks {
 #include <linux/security.h>
 #include <linux/static_call.h>
 #include <linux/lockdep.h>
+#include <linux/unwind_deferred.h>
 #include <asm/local.h>
 
 struct perf_callchain_entry {
@@ -829,6 +830,10 @@ struct perf_event {
 	struct irq_work			pending_disable_irq;
 	struct callback_head		pending_task;
 	unsigned int			pending_work;
+
+	unsigned int			pending_unwind_callback;
+	struct callback_head		pending_unwind_work;
+	struct rcuwait			pending_unwind_wait;
 
 	atomic_t			event_limit;
 
@@ -1652,7 +1657,7 @@ extern void perf_callchain_user(struct perf_callchain_entry_ctx *entry, struct p
 extern void perf_callchain_kernel(struct perf_callchain_entry_ctx *entry, struct pt_regs *regs);
 extern struct perf_callchain_entry *
 get_perf_callchain(struct pt_regs *regs, bool kernel, bool user,
-		   u32 max_stack, bool crosstask, bool add_mark);
+		   u32 max_stack, bool crosstask, bool add_mark, bool defer_user);
 extern int get_callchain_buffers(int max_stack);
 extern void put_callchain_buffers(void);
 extern struct perf_callchain_entry *get_callchain_entry(int *rctx);
